5629478a76fad12c9961a13683717cf8e3074c76
Prof-ThiagoOliveira <thiago.paula.oliveira@alumni.usp.br>
2023-12-15 14:26:15 +0000

    new blog post golem

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = null;
 
-  // ns-hugo:/home/thiago/Documents/2-Areas/Profissional/blog/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/v5/assets/js/wowchemy-utils.js
+  // ns-hugo:C:\Users\ThiagoOliveira\OneDrive - AbacusBio Ltd\Documents\2-Areas\blog\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
